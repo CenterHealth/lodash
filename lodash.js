b7116f9c9c8f6a2964c68b64fa50dce75af3d1ef
Julian Laval <hello@julianlaval.com>
2020-11-19 19:15:22 +0000

    Added _.isDefined

--- lodash.js
+++ lodash.js
@@ -12226,6 +12226,27 @@
     }
 
     /**
+     * Checks if `value` is not `undefined`.
+     *
+     * @static
+     * @since 0.1.0
+     * @memberOf _
+     * @category Lang
+     * @param {*} value The value to check.
+     * @returns {boolean} Returns `true` if `value` is not `undefined`, else `false`.
+     * @example
+     *
+     * _.isDefined(void 0);
+     * // => false
+     *
+     * _.isDefined(null);
+     * // => true
+     */
+    function isDefined(value) {
+      return value !== undefined;
+    }
+
+    /**
      * Checks if `value` is classified as a `WeakMap` object.
      *
      * @static
